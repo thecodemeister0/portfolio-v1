d75e525a8280d50729702f0b75c4b851d8480ca4
Austin Woodall <austin.woodall72@gmail.com>
2024-12-25 18:09:36 -0600

    remove sticky header back drop on lg screens

--- src/app/components/sticky-header.tsx
+++ src/app/components/sticky-header.tsx
@@ -1,10 +1,8 @@
 export function StickyHeader({ title }: { title: string }) {
   return (
-    <div className="sticky top-0 z-20 mb-4 py-4 bg-slate-900/75 backdrop-blur">
+    <div className="sticky top-0 z-20 mb-4 py-4 bg-slate-900/75 backdrop-blur lg:sr-only ">
       <h2
-        className={
-          "text-sm uppercase tracking-widest text-slate-200 font-bold lg:sr-only"
-        }
+        className={"text-sm uppercase tracking-widest text-slate-200 font-bold"}
       >
         {title}
       </h2>
